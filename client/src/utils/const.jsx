8024966bcf246d6e7d78bf08243fc5c6c63e1747
User <huseynovali612@gmail.com>
2023-10-19 14:12:31 +0400

    config

--- client/src/utils/const.jsx
+++ client/src/utils/const.jsx
@@ -1,6 +1,14 @@
 import comp from "../assets/image 42.png";
 import comp1 from "../assets/image 29.png";
 
+import roccat from "../assets/roccat.png"
+import msi from '../assets/msi.png'
+import razer from '../assets/razer.png'
+import therm from '../assets/therm.png'
+import adata from '../assets/adata.png'
+import hp from '../assets/hp.png'
+import gigabyte from '../assets/gigabyte.png'
+
 export const cardComp = [
   {
     quality: 2,
@@ -48,3 +56,36 @@ export const popComp = [
     text: "EX DISPLAY : MSI Pro 16 Flex-036AU 15.6 MULTITOUCH All-In-On...",
   },
 ];
+
+export const compCategory=[
+  {
+    title:'Roccat',
+    photo: roccat
+  },
+  {
+    title:'msi',
+    photo: msi
+  },
+  {
+    title:'razer',
+    photo: razer
+  },
+  {
+    title:'therm',
+    photo: therm
+  },
+  {
+    title:'adata',
+    photo: adata
+  },
+  {
+    title:'hp',
+    photo: hp
+  },
+  {
+    title:'gigabyte',
+    photo: gigabyte
+  },
+
+
+]
