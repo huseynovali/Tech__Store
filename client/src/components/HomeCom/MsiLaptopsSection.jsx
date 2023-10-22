4b35db51519d4c6fcf4f6b56bb3da14ae9b35f1a
User <huseynovali612@gmail.com>
2023-10-22 23:50:10 +0400

    config

--- client/src/components/HomeCom/MsiLaptopsSection.jsx
+++ client/src/components/HomeCom/MsiLaptopsSection.jsx
@@ -4,6 +4,7 @@ import { msiLaptops } from "../../utils/const";
 import { useState } from "react";
 import { Tab } from "@headlessui/react";
 import classNames from "classnames";
+import TabComponent from "./TabComponent";
 
 function MsiLaptopsSection() {
   let [categories] = useState({
@@ -16,44 +17,9 @@ function MsiLaptopsSection() {
   return (
     <div>
       <div className="w-full">
-        {console.log(categories)}
-        <Tab.Group>
-          <Tab.List className="flex ">
-            {Object.values(categories).map((category) => (
-              <Tab
-                key={category}
-                className={({ selected }) =>
-                  classNames(
-                    " text-sm font-medium  text-[#838383] px-4",
-                   
-                    selected
-                      ? "text-black underline outline-none underline-offset-4 "
-                      : "text-[#838383]"
-                  )
-                }
-              >
-                {category}
-              </Tab>
-            ))}
-          </Tab.List>
-          <Tab.Panels className="w-full">
-            <div className="py-5">
-              {Object.keys(categories).map((item, idx) => {
-                return (
-                  <Tab.Panel
-                    key={idx}
-                    className={classNames(
-                      "rounded-xl bg-white p-3",
-                      "ring-white ring-opacity-60 ring-offset-2 ring-offset-blue-400 focus:outline-none focus:ring-2"
-                    )}
-                  >
-                    <CompCategoryList data={msiLaptops[item]} />
-                  </Tab.Panel>
-                );
-              })}
-            </div>
-          </Tab.Panels>
-        </Tab.Group>
+       <TabComponent categories={categories} compType={msiLaptops}>
+       <CompCategoryList  />
+       </TabComponent>
       </div>
     </div>
   );
---
+++ client/src/components/HomeCom/TabComponent.jsx
@@ -0,0 +1,56 @@
+import React from "react";
+import { useState } from "react";
+import { Tab } from "@headlessui/react";
+import classNames from "classnames";
+function TabComponent({ children, categories, compType }) {
+  return (
+    <div>
+      <Tab.Group>
+        <Tab.List className="flex ">
+          {Object.values(categories).map((category) => (
+            <Tab
+              key={category}
+              className={({ selected }) =>
+                classNames(
+                  " text-sm font-medium  text-[#838383] px-4",
+
+                  selected
+                    ? "text-black underline outline-none underline-offset-4 "
+                    : "text-[#838383]"
+                )
+              }
+            >
+              {category}
+            </Tab>
+          ))}
+        </Tab.List>
+        <Tab.Panels className="w-full">
+          <div className="py-5">
+            {Object.keys(categories).map((item, idx) => {
+              return (
+                <Tab.Panel
+                  key={idx}
+                  className={classNames(
+                    "rounded-xl bg-white p-3",
+                    "ring-white ring-opacity-60 ring-offset-2 ring-offset-blue-400 focus:outline-none focus:ring-2"
+                  )}
+                >
+                  {React.Children.map(children, (child) => {
+                    if (React.isValidElement(child)) {
+                      return React.cloneElement(child, {
+                        data: compType[item],
+                      });
+                    }
+                    return child;
+                  })}
+                </Tab.Panel>
+              );
+            })}
+          </div>
+        </Tab.Panels>
+      </Tab.Group>
+    </div>
+  );
+}
+
+export default TabComponent;
