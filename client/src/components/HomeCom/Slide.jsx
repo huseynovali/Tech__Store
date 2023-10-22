5e476b83065f9ef85d47aeb764ad0d15ab21838b
User <huseynovali612@gmail.com>
2023-10-22 12:41:08 +0400

    create home page slider

---
+++ client/src/components/HomeCom/Slide.jsx
@@ -0,0 +1,32 @@
+import React from "react";
+import img1 from "../../assets/image 26.png";
+// Import Swiper React components
+import { Swiper, SwiperSlide } from "swiper/react";
+
+// Import Swiper styles
+import "swiper/css";
+import "swiper/css/navigation";
+
+// import required modules
+import { Navigation } from "swiper/modules";
+function Slide() {
+  return (
+    <div>
+      <Swiper navigation={true} modules={[Navigation]} className="mySwiper " >
+        <SwiperSlide>
+          <img src={img1} alt="" />
+        </SwiperSlide>
+        <SwiperSlide >Slide 2</SwiperSlide>
+        <SwiperSlide>Slide 3</SwiperSlide>
+        <SwiperSlide>Slide 4</SwiperSlide>
+        <SwiperSlide>Slide 5</SwiperSlide>
+        <SwiperSlide>Slide 6</SwiperSlide>
+        <SwiperSlide>Slide 7</SwiperSlide>
+        <SwiperSlide>Slide 8</SwiperSlide>
+        <SwiperSlide>Slide 9</SwiperSlide>
+      </Swiper>
+    </div>
+  );
+}
+
+export default Slide;
--- client/src/page/Home.jsx
+++ client/src/page/Home.jsx
@@ -1,8 +1,14 @@
 import React from 'react'
-
+import Slide from '../components/HomeCom/Slide'
+import Container from '../components/Container'
 function Home() {
   return (
-    <div>Home</div>
+    <div>
+      <Container>
+            <Slide/>
+      </Container>
+ 
+    </div>
   )
 }
 
