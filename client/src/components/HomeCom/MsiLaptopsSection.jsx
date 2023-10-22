d9c900ace8cd3dfb2f66388307cbf19e9b541c56
User <huseynovali612@gmail.com>
2023-10-22 23:40:54 +0400

    config style tab link

--- client/src/components/HomeCom/MsiLaptopsSection.jsx
+++ client/src/components/HomeCom/MsiLaptopsSection.jsx
@@ -18,17 +18,17 @@ function MsiLaptopsSection() {
       <div className="w-full">
         {console.log(categories)}
         <Tab.Group>
-          <Tab.List className="flex space-x-1 rounded-xl bg-blue-900/20 p-1">
+          <Tab.List className="flex ">
             {Object.values(categories).map((category) => (
               <Tab
                 key={category}
                 className={({ selected }) =>
                   classNames(
-                    "w-full rounded-lg py-2.5 text-sm font-medium leading-5 text-blue-700",
-                    "ring-white ring-opacity-60 ring-offset-2 ring-offset-blue-400 focus:outline-none focus:ring-2",
+                    " text-sm font-medium  text-[#838383] px-4",
+                   
                     selected
-                      ? "bg-white shadow"
-                      : "text-blue-100 hover:bg-white/[0.12] hover:text-white"
+                      ? "text-black underline outline-none underline-offset-4 "
+                      : "text-[#838383]"
                   )
                 }
               >
