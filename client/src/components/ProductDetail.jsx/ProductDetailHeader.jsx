f0df855efb9c51398f1e8d5807f3847bbc20dc10
User <huseynovali612@gmail.com>
2023-10-24 16:07:54 +0400

    create number input

--- client/src/components/ProductDetail.jsx/ProductDetailHeader.jsx
+++ client/src/components/ProductDetail.jsx/ProductDetailHeader.jsx
@@ -19,15 +19,52 @@ function ProductDetailHeader() {
           </div>
           <div className="right">
             <div className="cound__card flex items-center justify-center">
-              <p>On Sale from $3,299.00</p>
-              <div className="number__input__group">
+              <p className="text-sm ">On Sale from <span className="font-semibold">$3,299.00</span></p>
+              <div className="number__input__group relative ml-2">
                 <input
                   type="number"
                   min={0}
                   value={compCound}
                   onChange={handleInputChange}
-                  className="border-none outline-none bg-[#F5F7FF] w-[70px] h-[50px] py-2 px-3"
+                  className="border-none outline-none bg-[#F5F7FF] w-[70px] h-[50px] py-2 px-3 "
                 />
+                <div className="buttons__number__input  absolute right-0 top-0 max-w-min h-full">
+                  <div className="top__btn z-10 h-1/2 flex items-center">
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      width="16"
+                      height="16"
+                      viewBox="0 0 16 16"
+                      fill="none"
+                    >
+                      <path
+                        d="M6 9L8 7L10 9"
+                        stroke="#A2A6B0"
+                        stroke-width="1.6"
+                        stroke-linecap="round"
+                      />
+                    </svg>
+                  </div>
+                  <div className="bottom__btn z-10 h-1/2 flex items-center">
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      width="16"
+                      height="16"
+                      viewBox="0 0 16 16"
+                      fill="none"
+                    >
+                      <path
+                        d="M10 7L8 9L6 7"
+                        stroke="#A2A6B0"
+                        stroke-width="1.6"
+                        stroke-linecap="round"
+                      />
+                    </svg>
+                  </div>
+                </div>
+              </div>
+              <div className="pay__buttons">
+                
               </div>
             </div>
           </div>
