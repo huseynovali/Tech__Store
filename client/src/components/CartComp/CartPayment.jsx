5883442e1095ef861c2423c0c57333596bac6af6
User <huseynovali612@gmail.com>
2023-11-09 23:50:41 +0400

    config shopping cart

--- client/src/components/CartComp/CartPayment.jsx
+++ client/src/components/CartComp/CartPayment.jsx
@@ -40,6 +40,37 @@ function CartPayment() {
           </>
         )}
       </Disclosure>
+      <Disclosure>
+        {({ open }) => (
+          <>
+            <Disclosure.Button className=" w-full  text-left flex py-5 justify-between">
+              <div>
+                <p className="pb-2">Apply Discount Code</p>
+              </div>
+              <div className={`${open ? "rotate-180 transform" : ""} h-5 w-5 transition-transform`}>
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="16"
+                  height="15"
+                  viewBox="0 0 16 15"
+                  fill="none"
+                >
+                  <path
+                    d="M6 9.20209L8 7.20209L10 9.20209"
+                    stroke="black"
+                    stroke-width="1.6"
+                    stroke-linecap="round"
+                  />
+                </svg>
+              </div>
+            </Disclosure.Button>
+            <Disclosure.Panel className="px-4 pt-4 pb-2 text-sm text-gray-500">
+              If you're unhappy with your purchase for any reason, email us
+              within 90 days and we'll refund you in full, no questions asked.
+            </Disclosure.Panel>
+          </>
+        )}
+      </Disclosure>
     </div>
   );
 }
